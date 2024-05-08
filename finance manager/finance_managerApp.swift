a9880deb66b6d9d951b1738e17c156b19b57db46
hserhiis <serhiiswr1994@gmail.com>
2024-05-09 00:01:48 +0200

    Initial Commit

---
+++ finance manager/finance_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  finance_managerApp.swift
+//  finance manager
+//
+//  Created by Serhii Shapovalov on 2024-05-09.
+//
+
+import SwiftUI
+
+@main
+struct finance_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
